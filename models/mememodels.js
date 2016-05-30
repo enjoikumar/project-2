2578e6da8f63b62e3ee58fb501270eb858e12ace
Kamal Kumar <kamalkumar@Kamals-MacBook-Pro.local>
2016-05-30 14:43:08 -0400

    created model

---
+++ models/mememodels.js
@@ -0,0 +1,53 @@
+module.exports = [
+		{ 
+			id: 1,
+			name: "Doge",
+		  img: "https://upload.wikimedia.org/wikipedia/en/5/5f/Original_Doge_meme.jpg",
+		  about: "What this? How do?"
+		}, {
+			id: 2,
+			name: " Desk Flip Guy Rage ",
+			img: "https://cdn.meme.am/instances/500x/68593897.jpg",
+			about: "Used to illustrate rage and frustration",
+		}, {
+			id: 3,
+			name: "Over 900",
+			img: "https://cdn.meme.am/instances/500x/68593789.jpg",
+			about: "A reference to an episode of Dragon Ball Z, where the antagonist underestimates the protagonist's power level",
+		}, {
+			id: 4,
+			name: "Drake",
+			img: "https://s3.amazonaws.com/hiphopdx-production/2015/12/Drake-Stormtrooper.jpg",
+			about: "The Toronto will always be the butt of the joke. His memes often refer to his soft side",
+		}, {
+			id: 5,
+			name: "Spongebob",
+			img: "http://memesvault.com/wp-content/uploads/Spongebob-Meme-How-Tough-Am-I-22.jpg",
+			about: "The popular children's television has been used as a meme for years, and has no sign of letting down with various screen caps of episodes and use of wide characters.",
+		}, {
+			id: 6,
+			name: "One Does Not simply",
+			img: "https://cdn.meme.am/instances/500x/68593769.jpg",
+			about: "Popularized by the phrase 'one does not' "
+		}, {
+			id: 7,
+			name: "Brace Yourself",
+			img: "https://cdn.meme.am/instances/500x/68593769.jpg",
+			about: "A reference to Game of Thrones, this meme has been widely used to illustrate fear and awkwardness."
+		}, {
+			id: 8,
+			name:"Joe the Instructor",
+			img:"https://cdn.meme.am/instances/500x/68593942.jpg",
+			about:"Joe the instructor is about the antics of..well Joe the instructor."
+		},{
+			id:9,
+			name:"Jordan Crying Face",
+			img:"https://pics.onsizzle.com/581924093362061313-Twitter.png",
+			about:"Now everyone can get the Jordan crying meme. Taken from Michael Jordan's hall of fame induction, the face can be put on anyone or anything to dispense a feeling of loss or taking an L."
+		},{
+			id:10,
+			name:"Bad Luck Brian",
+			img:"https://cdn.meme.am/instances/500x/68594086.jpg",
+			about: "Basically, bad luck. Where nothing goes right."
+		}
+	];
